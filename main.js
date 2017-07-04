ed5f9607d83d9c404936af4056287ef1fbca0212
A Different Earth <adifferentearth@gmail.com>
2017-07-03 20:02:53 -0400

    My first files

---
+++ main.js
@@ -0,0 +1,21 @@
+function clicked(){
+	
+	var user = document.getElementById('username');
+	var pass = document.getElementById('password');
+
+	var coruser = "test";
+	var corpass = "123";
+
+	if(user.value == coruser) & (pass.value == corpass){
+
+		window.alert("You are now logged in");
+		
+	} else {
+
+		window.alert("Incorrect Password or Username");
+	}
+	else {
+		window.alert("Incorrect Password or Username");
+	}
+}
+
